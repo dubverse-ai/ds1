27a1c40da24c5e7571c020b5b3a62a0b52dd813d
Kushal Patankar <kush@dubverse.ai>
2024-01-18 20:44:34 +0530

    ...

--- ds1/__init__.py
+++ ds1/__init__.py
@@ -1,10 +1,4 @@
-import logging
-
 from .client import Client
 from .core.users import Users
 
-
-c = Client('hack4@dubverse.ai', 'localhost')
-u = Users(c)
-print(u.client)
-
+__all__ = ["Client", "Users"]
