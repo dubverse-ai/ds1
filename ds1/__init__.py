386d080d21ed5c3378a71ee4aa69bb593bd1909c
Kushal Patankar <kush@dubverse.ai>
2024-01-31 12:47:33 +0530

    update: export auth

--- ds1/__init__.py
+++ ds1/__init__.py
@@ -2,5 +2,6 @@ from ds1.client import Client
 from ds1.core.user import User
 from ds1.core.subscription import Subscripton
 from ds1.core.config import Config
+from ds1.core.auth import Auth
 
-__all__ = ["Client", "User", "Subscripton", "Config"]
+__all__ = ["Client", "User", "Subscripton", "Config", "Auth"]
