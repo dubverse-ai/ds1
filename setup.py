16a727765947d7f6193391d6e355cc4f5158d824
Harshil Mehta <harshil.mehta@dubverse.ai>
2024-08-13 14:14:59 +0530

    fix: fix for backport building wheel

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+install_requires = [
+    "requests>=2.0",
+    "cachetools",
+]
+
+extras_require = {
+    ":python_version<'3.9'": ["backports.zoneinfo"],
+}
 
 setup(
     name="ds1",
     version="0.1.18",
-    author="peritissimus",
+    author="dubverse",
     description="Dubverse SDK for Python",
     packages=find_packages(),  # Automatically discover and include all packages
-    install_requires=["requests>=2.0", "cachetools"],
+    install_requires=install_requires,
+    extras_require=extras_require,
     python_requires=">=3.6",
     classifiers=[
         "Development Status :: 1 - Planning",
@@ -17,6 +27,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",  # Adjust the license as needed
     ],
     project_urls={
