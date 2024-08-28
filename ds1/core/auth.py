eb4f020f9b004bfd791b4ba3ad555a1bc67b619a
Harshil Mehta <harshil.mehta@dubverse.ai>
2024-08-28 11:19:35 +0530

    feat(tts_api): cached auth response for 5 min instead 1 day
    Closes #DW-1906

--- ds1/core/auth.py
+++ ds1/core/auth.py
@@ -12,7 +12,7 @@ class Auth:
             self.base_url = core_url + URL.VERSION
         else:
             self.base_url = URL.BASE_URL + URL.VERSION
-        self.cache = TTLCache(maxsize=1, ttl=86400)  # 86400 seconds = 1 day
+        self.cache = TTLCache(maxsize=1, ttl=300)  # 300 seconds = 5 minutes
 
     def verify_token(self, token):
         cached_response = self.cache.get(token)
@@ -45,6 +45,10 @@ class Auth:
             raise DubverseError(f"Error Authorizing Client: {str(e)}")
 
     def verify_api(self, api_key):
+        cached_response = self.cache.get(api_key)
+        if cached_response:
+            return cached_response
+
         url = self.base_url + URL.TTS_SERVICE + URL.TTS_VERIFY_KEY
         payload = {"api_key": api_key}
 
@@ -52,6 +56,7 @@ class Auth:
             response = requests.post(url, json=payload)
             response.raise_for_status()
             response_json = response.json()
+            self.cache[api_key] = response_json
 
             return response_json
         except requests.exceptions.RequestException as e:
